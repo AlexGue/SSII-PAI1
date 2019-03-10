f3ed0ed36c6601916b8eaf28262cca8aee8abb11
Alejandro <alexgue404@gmail.com>
2019-03-10 02:09:49 +0100

    Config file working and added groupal checking

--- main.py
+++ main.py
@@ -3,14 +3,41 @@ import sys
 import hashlib
 import time
 import datetime
+import json
 # BUF_SIZE is totally arbitrary, change for your app!
 BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+configFile = "Config.json"
+
+def leeFicheros(file):
+    f = open (file, "r",encoding='utf-8')
+    data = json.load(f)
+    ficheros=data['ficheros']
+    return ficheros
+
+def leeAlgoritmo(file):
+    f = open (file, "r",encoding='utf-8')
+    data = json.load(f)
+    algoritmo=data['algoritmo']
+    return algoritmo
+
+def leeFicheroIncidencias(file):
+    f = open (file, "r",encoding='utf-8')
+    data = json.load(f)
+    ficheroIncidencias=data['ficheroIncidencias']
+    return ficheroIncidencias
+
+def leeTiempoComprobacion(file):
+    f = open (file, "r",encoding='utf-8')
+    data = json.load(f)
+    tiempoComprobacion=data['tiempoComprobacion']
+    return int(tiempoComprobacion)
 
 
-filesToCheck = ["C:/Users/Ale/Desktop/Universidad/SSII/test1.txt", "C:/Users/Ale/Desktop/Universidad/SSII/test2.txt"]
-algorithm = "sha256"
-repeatTime = 5.0
+filesToCheck = leeFicheros(configFile)
+repeatTime = leeTiempoComprobacion(configFile)
 now = datetime.datetime.now()
+algorithm = leeAlgoritmo(configFile)
+
 
 
 
@@ -23,18 +50,27 @@ def storeInLogFile(line):
         fileLog = open(fn, 'a')
     fileLog.write(line + '\n')
     
-def checkFiles():
-    for file in filesToCheck:
+def checkFiles(): 
+    hashes = []
+    nowTime = datetime.datetime.now()
+    storeInLogFile("Starting file check | " + str(nowTime) )
+    for fileH in filesToCheck:
         hashR = getattr(hashlib, algorithm)()
-        with open(file, 'rb') as f:
+        with open(fileH, 'rb') as f:
             while True:
                 data = f.read(BUF_SIZE)
                 if not data:
                     break
-                hashR.update(data)
-        lineLog = ("SHA256: {0} of file {1}".format(hashR.hexdigest(), file))
+                hashFile = hashR.update(data)
+        lineLog = ("{0}|{2}| of file {1}".format(hashR.hexdigest(), fileH, algorithm))
+        hashes.append(hashR.hexdigest())
         print(lineLog)
         storeInLogFile(lineLog)
+    finalLog = str(hashes)+ str(nowTime)
+    hashAlg = getattr(hashlib, algorithm)()
+    hashAlg.update(finalLog.encode('utf-8'))
+    hashFinal = hashAlg.hexdigest()
+    storeInLogFile("Finished file check | " + hashFinal)
 
 def startChecks():
     starttime=time.time()
@@ -43,4 +79,10 @@ def startChecks():
         checkFiles()
         time.sleep(repeatTime - ((time.time() - starttime) % repeatTime))
 
+#Cambiar contenido de fichero de configuraci�n y ruta de archivos
+
+
+
 startChecks()
+
+ficherosToCheck=leeFicheros("Config.json")
